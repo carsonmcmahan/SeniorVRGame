75bc44a3c4501ce00aacdc8a5118c20ee233ef06
Carson McMahan <carson.mcmahan@gmail.com>
2023-12-04 15:34:13 -0700

    Started Adding Audio
    Added audio for the tutorial level.

--- Architecture Simulator/Assets/Scripts/Tutorial/TurtorialManager.cs
+++ Architecture Simulator/Assets/Scripts/Tutorial/TurtorialManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Tutorials.Core.Editor;
 using UnityEngine;
 
 public class TurtorialManager : MonoBehaviour
@@ -8,17 +9,46 @@ public class TurtorialManager : MonoBehaviour
     public AudioSource tutorialAudioSource;
     public AudioClip[] tutorialAudio;
 
-    // Start is called before the first frame update
+    int numOfPickUps = 0;
+    int hubButtonPresses = 0;
+
+    private void Start()
+    {
+        tutorialAudioSource.PlayOneShot(tutorialAudio[0]);
+        numOfPickUps = 0;
+        hubButtonPresses = 0;
+    }
 
     public void StartTutorial(GameObject canvas)
     {
         canvas.SetActive(false);
 
         // play audio 
+        tutorialAudioSource.PlayOneShot(tutorialAudio[1]);
     }
 
     public void SpawnHouse(GameObject house)
     {
         house.SetActive(true);
+    }
+
+    public void PickedUpTablet()
+    {
+        numOfPickUps++;
+
+        if (numOfPickUps == 1)
+        {
+            tutorialAudioSource.PlayOneShot(tutorialAudio[2]);
+        }
+    }
+
+    public void AudioForMainHubTeleport()
+    {
+        hubButtonPresses++;
+
+        if(hubButtonPresses == 1)
+        {
+            tutorialAudioSource.PlayOneShot(tutorialAudio[3]);
+        }
     }
 }
